c5a5abb9e76e37190de1d142862fae974a74c6de
zionHuang <hjszion@gmail.com>
2019-09-18 16:39:43 -0700

    finished requirejs

--- js/customScripts/jqueryFile.js
+++ js/customScripts/jqueryFile.js
@@ -1,4 +1,4 @@
-define(['jquery', 'angular', 'methods'],function($, methods){
+define(['jquery', 'methods'],function($, methods){  //denpends on jqueyr and methods
     $('#clickMe').click(function(){
         methods.changeHTML('I was clicked!');
     })
